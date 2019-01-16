300506d9a7cca322c34e715e7785d87712f840ac
Rahul <rahulrpa@in.ibm.com>
2019-01-16 01:53:50 -0800

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+export default class extends React.Component {
+  render() {
+    return <div style={{ color: "blue" }}>Hello Next.js</div>;
+  }
+}
---
+++ server/server-dev.js
@@ -0,0 +1,59 @@
+const http2 = require("http2");
+const {
+  pathWrapper,
+  defaultHandlerWrapper,
+  nextHandlerWrapper
+} = require("./next-wrapper");
+const { parse } = require("url");
+const Hapi = require("hapi");
+const fs = require("fs");
+const next = require("next");
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+const options = {
+  key: fs.readFileSync("./ssl_keys/server.key"),
+  cert: fs.readFileSync("./ssl_keys/server.crt")
+};
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const server = new Hapi.Server({
+  listener: http2.createSecureServer(options),
+  host: "localhost",
+  port,
+  tls: true
+});
+
+app.prepare().then(async () => {
+  server.route({
+    method: "GET",
+    path: "/a",
+    handler: pathWrapper(app, "/a")
+  });
+
+  server.route({
+    method: "GET",
+    path: "/b",
+    handler: pathWrapper(app, "/b")
+  });
+
+  server.route({
+    method: "GET",
+    path: "/_next/{p*}" /* next specific routes */,
+    handler: nextHandlerWrapper(app)
+  });
+
+  server.route({
+    method: "GET",
+    path: "/{p*}" /* catch all route */,
+    handler: defaultHandlerWrapper(app)
+  });
+
+  try {
+    await server.start();
+    console.log(`> Ready on https://localhost:${port}`);
+  } catch (error) {
+    console.log("Error starting server");
+    console.log(error);
+  }
+});
